b2371af4008302a01ba378d364412d11796e9e31
MaximilianKnick <njozwiak82@gmail.com>
2021-05-01 16:21:24 -0400

    Updated generating_data
    More small edits. ~should~ be good to go now. Will confirm after I install MATLAB 2020b

--- generating_data.m
+++ generating_data.m
@@ -11,22 +11,30 @@ clc
 tic
 %% Variables
 
-% *** need to find reasonable stiffness values and values for muscle atrophy ***
-% exo_stiff_range = 180000 * 0.8:0.1:1.2;   % Range of tendon stiffness values
-% grav_range = 9.81 * 0.5:0.1:2;
-% atrophy = 0.8:0.1:1.2 * [6000; -0.45];
-exo_stiff_range = 180000 * 1;   % Range of tendon stiffness values
+% ranges of variables for traversing through the for loops
+exo_stiff_range = 180000 * 1;
+grav_range = 2 :2:24;
+fmax_range = [6000];
+vmax_range = [-0.45];
+act_range = [1];
+
+% test values (comment out and use ^above^ definitions for actual data)
+exo_stiff_range = 180000 * 1;
 grav_range = 9.81 * 1;
 fmax_range = [6000];
 vmax_range = [-0.45];
 act_range = [1];
+
+% values of variables for when they are being held constant over the
+% iterations
 fmax = 6000;
 vmax = -0.45;
 act = 1;
+grav = 9.81;
 
 
 %% F_MAX
-for a = 1:length(grav_range)    
+parfor a = 1:length(grav_range)    
     grav = grav_range(a);
     for b = 1:length(fmax_range)
         fmax = fmax_range(b);
@@ -41,7 +49,7 @@ for a = 1:length(grav_range)
                 set_param('FullHopper_passiveExo/Vmax_mus (m\s)','Value',num2str(vmax));    %Setting muscle parameters in model
                 set_param('FullHopper_passiveExo/pulse','Amplitude',num2str(act));    %Setting muscle parameters in model
                 % FILE NAME
-                name = sprintf('exoData_grav_%s_fmax_%s_stiff_%s.mat', num2str(grav),num2str(vmax),num2str(stiff));
+                name = sprintf('exoData_grav_%s_fmax_%s_stiff_%s.mat', num2str(grav),num2str(fmax),num2str(stiff));
                 set_param('FullHopper_passiveExo/To File','FileName', name);  %Setting File Name                
                 
                 simout{a} = sim('FullHopper_passiveExo.slx');                            %Simulates model
@@ -112,6 +120,33 @@ parfor a = 1:length(grav_range)
     end
 end
 
-% test change
+%% F_MAX and V_MAX
+parfor a = 1:length(fmax_range)    
+    fmax = fmax_range(a);
+    for b = 1:length(vmax_range)
+        vmax = vmax_range(b);
+        for c = 1:size(exo_stiff_range)
+            stiff = exo_stiff_range(c);
+            try
+                load_system('FullHopper_passiveExo.slx');                                %Loading model                
+                % CHANGE MODEL PARAMETERS
+                set_param('FullHopper_passiveExo/stiffness','Value',num2str(stiff));    %Setting exo stiffness in model
+                set_param('FullHopper_passiveExo/LoadDynamics/gravity','Value',num2str(grav));    %Setting gravity constant in model
+                set_param('FullHopper_passiveExo/Fmax_mus (N)','Value',num2str(fmax));    %Setting muscle parameters in model
+                set_param('FullHopper_passiveExo/Vmax_mus (m\s)','Value',num2str(vmax));    %Setting muscle parameters in model
+                set_param('FullHopper_passiveExo/pulse','Amplitude',num2str(act));    %Setting muscle parameters in model
+                % FILE NAME
+                name = sprintf('exoData_fmax_%s_vmax_%s_stiff_%s.mat', num2str(fmax),num2str(vmax),num2str(stiff));
+                set_param('FullHopper_passiveExo/To File','FileName', name);  %Setting File Name                
+                
+                simout{a} = sim('FullHopper_passiveExo.slx');                            %Simulates model
+                close_system('FullHopper_passiveExo.slx',0);                             %Closes model                
+            catch err
+                err.identifier
+                'caught error 4'
+            end
+        end
+    end
+end
 
 toc
