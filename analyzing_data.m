105a69530b5ac82ca29b3f926a216e3603312f37
MaximilianKnick <njozwiak82@gmail.com>
2021-05-03 15:03:13 -0400

    updated analyzing data for work (using vmax)
    also changed gravity

--- analyzing_data.m
+++ analyzing_data.m
@@ -6,13 +6,13 @@
 % based on exoskeleton spring stiffness and muscle atrophy
 %% Clearing variables and figures
 close all
-clear
+clear all
 clc
 tic
 %% Variables
 
 exo_stiff_range = linspace(100,200000,5);
-grav_range = 2:2:24;
+grav_range = 2:1:14;
 fmax_range = 6000 * [0.7 0.85 1 1.15 1.3];
 vmax_range = -0.45 * [0.5 0.75 1 1.25 1.5];
 act_range = linspace(0.5,1,5);
@@ -20,10 +20,10 @@ act_range = linspace(0.5,1,5);
 
 %% 3 c.
 
-grav_range   = 2:2:24;
+grav_range   = 2:1:14;
 exo_stiff_range = linspace(100,200000,5);
 
-fmax = fmax_range(3);
+vmax = vmax_range(3);
 
 W_mtu_net = zeros(5, length(grav_range));
 W_mtu_pos = zeros(5, length(grav_range));
@@ -37,16 +37,19 @@ for j = 1:length(grav_range)
         
         exo_stiff = exo_stiff_range(i);
         
-        fid = sprintf('exoData_grav_%s_fmax_%s_stiff_%s.mat', num2str(grav),num2str(fmax),num2str(exo_stiff));
+        fid = sprintf('exoData_grav_%s_vmax_%s_stiff_%s.mat', num2str(grav),num2str(vmax),num2str(exo_stiff));
 
         if exist(fid, 'file') == 2          % Checking if file exists      
 
             d = load(fid);          % Loads data from file
             t = d.data.time;        
             data = d.data.data;
-            t0 = t>=1.4 & t<=1.8;
+            act = data(:,1);
+            crossings = find(diff(act) > 0);
+            start = crossings(end-1);
+            last = crossings(end);
 
-            P = data(t0,20);
+            P = data(start:last,20);
             W_mtu_net(i, j) = sum(P);
             W_mtu_pos(i, j) = sum(P(P>0));
             W_mtu_neg(i, j) = sum(P(P<0));
@@ -59,7 +62,7 @@ figure(3)
 subplot(3,1,1);
 bar(grav_range, W_mtu_net);
 h = bar(grav_range, W_mtu_net);
-legend(h, {'45000', '90000', '180000', '360000', '720000'}, 'Location','southwest');
+% legend(h, mat2cell(exo_stiff_range), 'Location','southwest');
 legend('boxoff')
 ylabel('Net Mechanical Work (J)');
 
@@ -69,7 +72,7 @@ ylabel('Positive Mechanical Work (J)');
 
 subplot(3,1,3);
 bar(grav_range, W_mtu_neg); 
-xlabel ('Muscle simulation onset phase (s)')
+xlabel ('Gravity [m/s^2]')
 ylabel('Negative Mechanical Work (J)');
 
 sgtitle('Q.3 c.')
