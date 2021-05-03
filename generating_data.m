01414a1059b2db95a902d863d08d15cf1f42e0fd
ishratarora <ishrat.arora1997@gmail.com>
2021-05-03 20:17:02 +0530

    updates to fixed g block

--- generating_data.m
+++ generating_data.m
@@ -122,36 +122,46 @@ toc
 
 % grav = 3.7;   % Mercury
 % grav = 8.87;  % Venus
-grav = 9.81;  % Earth
+% grav = 9.81;  % Earth
 % grav = 1.62;  % Moon
 % grav = 3.72;  % Mars
 % grav = 24.8;  % Jupiter
 % grav = 10.44; % Saturn
 % grav = 8.8;   % Uranus
 % grav = 11.15; % Neptune
+
+planets = {'Earth', 'Moon', 'Mars'};
+planets_grav = [9.81, 1.62, 3.72];
+
 tic
-parfor a = 1:length(fmax_range)    
-    fmax = fmax_range(a);
-    for b = 1:length(vmax_range)
-        vmax = vmax_range(b);
-        for c = 1:length(exo_stiff_range)
-            stiff = exo_stiff_range(c);
-            try
-                load_system(file_name);                                %Loading model                
-                % CHANGE MODEL PARAMETERS
-                set_param('FullHopper_passiveExo_PWM/stiffness','Value',num2str(stiff));    %Setting exo stiffness in model
-                set_param('FullHopper_passiveExo_PWM/LoadDynamics/gravity','Value',num2str(grav));    %Setting gravity constant in model
-                set_param('FullHopper_passiveExo_PWM/Fmax_mus (N)','Value',num2str(fmax));    %Setting muscle parameters in model
-                set_param('FullHopper_passiveExo_PWM/Vmax_mus (m\s)','Value',num2str(vmax));    %Setting muscle parameters in model
-                % FILE NAME
-                name = sprintf('exoData_fmax_%s_vmax_%s_stiff_%s.mat', num2str(fmax),num2str(vmax),num2str(stiff));
-                set_param('FullHopper_passiveExo_PWM/To File','FileName', name);  %Setting File Name                
-                
-                simout{a} = sim(file_name);                            %Simulates model
-                close_system(file_name,0);                             %Closes model                
-            catch err
-                err.identifier
-                'caught error 4'
+parfor a = 1:length(planets) 
+    
+    planet = planets(a);
+    planet_grav = planets_grav(a);
+    
+    for b = 1:length(fmax_range)
+        fmax = fmax_range(b);
+        for c = 1:length(vmax_range)
+            vmax = vmax_range(c);
+            for d = 1:length(exo_stiff_range)
+                stiff = exo_stiff_range(d);
+                try
+                    load_system(file_name);                                %Loading model                
+                    % CHANGE MODEL PARAMETERS
+                    set_param('FullHopper_passiveExo_PWM/stiffness','Value',num2str(stiff));    %Setting exo stiffness in model
+                    set_param('FullHopper_passiveExo_PWM/LoadDynamics/gravity','Value',num2str(planet_grav));    %Setting gravity constant in model
+                    set_param('FullHopper_passiveExo_PWM/Fmax_mus (N)','Value',num2str(fmax));    %Setting muscle parameters in model
+                    set_param('FullHopper_passiveExo_PWM/Vmax_mus (m\s)','Value',num2str(vmax));    %Setting muscle parameters in model
+                    % FILE NAME
+                    name = sprintf('exoData_%s_fmax_%s_vmax_%s_stiff_%s.mat', string(planet),num2str(fmax),num2str(vmax),num2str(stiff));
+                    set_param('FullHopper_passiveExo_PWM/To File','FileName', name);  %Setting File Name                
+
+                    simout{a} = sim(file_name);                            %Simulates model
+                    close_system(file_name,0);                             %Closes model                
+                catch err
+                    err.identifier
+                    'caught error 4'
+                end
             end
         end
     end
